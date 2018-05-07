a256d0c6cc15eb8154b8ae64684382a690bad97a
lcj <1119884256@qq.com>
2018-05-07 16:34:51 +0800

    上传头像

--- src/servlet/UploadHeadImg.java
+++ src/servlet/UploadHeadImg.java
@@ -20,7 +20,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 /**
  * Servlet implementation class UploadHeadImg
  */
-@WebServlet("/UploadHeadImg")
+@WebServlet("/aaaaaa")
 public class UploadHeadImg extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ServletContext sc;
